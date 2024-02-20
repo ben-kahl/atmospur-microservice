6e06761adfdf4c680a63a6bad81875b8187c39ec
Ben Kahl <ben.kahl2002@gmail.com>
2024-02-19 16:30:22 -0800

    Added basic Bokeh plot implementation

--- etl.py
+++ etl.py
@@ -1,5 +1,6 @@
-import requests, json, time, csv
-
+import requests, json, time, csv, random
+from bokeh.plotting import figure, show
+from datetime import datetime
 def get_aqi_data(external_data, internal_data, time_log):
     aqi_combined_json = {"outside-aqi": external_data,
                         "inside-aqi": internal_data, 
@@ -34,11 +35,30 @@ def log_aqi_data(aqi_data):
     csv_writer.writerow(aqi_data.values())
     data_log.close()
 
-test_data = {"outside-aqi": 20,
-             "inside-aqi": 30,
-             "time": "Wed Sep 1 10:45:13 2021"}
-#aqi_to_log = receive_aqi_data()
-log_aqi_data(test_data)
+def plot_aqi_data():
+    data_log = open('log.csv', 'r')
+    csv_reader = csv.reader(data_log)
+    aqi_data = list(csv_reader)
+    time = []
+    outside_aqi = []
+    inside_aqi = []
+    for row in aqi_data[1:]:
+        time.append(int(datetime.strptime(row[2], "%a %b %d %H:%M:%S %Y").timestamp()))
+        outside_aqi.append(int(row[0]))
+        inside_aqi.append(int(row[1]))
+    plot = figure(title="AQI Data", x_axis_type="datetime", x_axis_label='Time', y_axis_label='AQI', x_range=(time[0], time[-1]), background_fill_color="#232627")
+    plot.line(time, outside_aqi, legend_label="Outside AQI", line_width=2)
+    plot.line(time, inside_aqi, legend_label="Inside AQI", line_width=2, line_color="red")
+    show(plot)
+
+
+for x in range(10):
+    test_data = {"outside-aqi": random.randint(15, 50),
+             "inside-aqi": random.randint(15, 50),
+             "time": time.asctime(time.localtime())}
+    log_aqi_data(test_data)
+    time.sleep(1)
+plot_aqi_data()
 # while True:
 #     aqi_data = receive_aqi_data()
 #     log_aqi_data(aqi_data)
